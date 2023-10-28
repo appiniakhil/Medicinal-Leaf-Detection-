44a32f53c1b1753e84bbe780bb8ea16b7c209f0c
Appini Akhil <91030529+appiniakhil@users.noreply.github.com>
2023-10-28 20:12:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from PIL import Image
 import cv2
 import numpy as np
 import io
+from io import BytesIO
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -122,6 +123,13 @@ def pred_image():
     file_img = request.files['file']
     img_data = request.files['file'].read()  # Changed variable name to img_data
     img = Image.open(io.BytesIO(img_data))
+# Check if the image format is PNG, and if so, convert it to JPG
+    if img.format == 'PNG':
+        # Convert PNG to RGB mode and save as JPG
+        img = img.convert('RGB')
+        img_data = BytesIO()
+        img.save(img_data, 'JPEG')
+        img_data = img_data.getvalue()
     
     # to remove background
     image1 = np.array(img)
@@ -149,7 +157,7 @@ def pred_image():
 
     # Set the non-leaf part to white
     result[np.where((result == [0, 0, 0]).all(axis=2))] = [255, 255, 255]
-    
+
     img2 = result
     # Saving the uploaded image to the UPLOAD_FOLDER
     filename = secure_filename(file_img.filename)
@@ -162,143 +170,148 @@ def pred_image():
     print("Prediction: ", species, flush=True)
     print(f'Confidence: {confidence:.2f}')
     
-    
-    # Web Scraping
-    # Web Scraping
-    # Web Scraping https://tropical.theferns.info/viewtropical.php?id=Carissa+carandas
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }
-    url1 = 'https://pfaf.org/user/Plant.aspx?LatinName={}'.format(sci_name)
-    url2 = 'https://tropical.theferns.info/viewtropical.php?id={}'.format(sci_name)
-    page = requests.get(url1,headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # print(soup)
-    tables=soup.find_all('table')
-    # print(tables[4])
+    if confidence > 0.66:
+        # Web Scraping
+        # Web Scraping
+        # Web Scraping https://tropical.theferns.info/viewtropical.php?id=Carissa+carandas
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+        url1 = 'https://pfaf.org/user/Plant.aspx?LatinName={}'.format(sci_name)
+        url2 = 'https://tropical.theferns.info/viewtropical.php?id={}'.format(sci_name)
+        page = requests.get(url1,headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # print(soup)
+        tables=soup.find_all('table')
+        # print(tables[4])
 
-    #composition 
-    try:
-        composition=list()
-        compositiontable=tables[4]
-        coldata=(compositiontable.find_all('tr'))
-        for row in coldata:
-            rowdata=(row.find_all('td'))
-            individualrowdata=[data.text.strip() for data in rowdata]
-            composition.append(individualrowdata)
-        # print(composition)
-        dry_weight_index = composition.index(['Figures in grams (g) or miligrams (mg) per 100g of food.', ''])
-
-        # Extract the relevant data
-        desired_data = composition[dry_weight_index + 1:]
-    
-        # Format and print the data
-        # for line in desired_data:
-        #     print('\n'.join(line))
-
-        formatted_data = "\n".join(["\n".join(line) for line in desired_data])
-        # print(formatted_data)
-        # Check if the element with class 'boots2' exists
-        string_length = len(formatted_data)
-
-        # Calculate the number of characters to keep (1/3 of the length)
-        characters_to_keep = string_length // 3
-
-        # Slice the string to keep only the first 1/3 of the characters
-        formatted_data = formatted_data[:characters_to_keep]
-        formatted_data =formatted_data.replace("Reference: [ ]Notes", "")
-    except IndexError:
-        formatted_data = comp[(species)]
-        # if formatted_data == "Details Not Found":
-        #     formatted_data == comp[(species)]
-    except ValueError or KeyError:
-        formatted_data = comp[(species)]
-        # if formatted_data == "Details Not Found":
-        #     formatted_data == comp[(species)]
-    
-    # #medicinal props
-    # element = soup.find(class_='boots2')
-    # if element:
-    #     results = element.find('span')
-    #     resarr = list(results.descendants)
-    #     try:
-    #         medprop = resarr[-1]
-    #         medprop = re.sub('\[.*?\]', ' ', medprop)
-    #     except IndexError:
-    #         medprop = "No Medicinal Properties Available"
-    # else:
-    #     medprop = "No Medicinal Properties Available"
-
-
-    #medicinal props
-    #edible uses if available
-
-    element = soup.find(class_='boots2')
-    try:
-        if element:
-            results = element.find('span')
-            resarr = list(results.descendants)
-            try:
-                medprop = resarr[-1]
-                medprop = re.sub('\[.*?\]', ' ', medprop)
-            except IndexError:
-                medprop = "No Medicinal Properties Available"
-    except Exception as e:
-        print(f"Error parsing first website: {str(e)}")
-    try:
-        page2 = requests.get(url2,headers=headers)
-        soup2 = BeautifulSoup(page2.content, 'html.parser')
-        
-        pagetags=soup2.find('div',class_='PageBox')
-        
-        paragraphs = soup2.find_all('p')  # Assuming the text is within a <p> tag
-        for div in soup2.find_all('div', class_='ref'):
-            div.decompose()
-        text=""
-        for paragraph in paragraphs:
-            text=text+(paragraph.text)
-        
-        # print(text)
-        # Define a regular expression pattern to capture the desired text
-        pattern = r'Edible Uses(.+?)Medicinal(.+?)Other Uses'
-
-        # Use re.search to find the first match
-        match = re.search(pattern, text, re.DOTALL)
-
-        # If a match is found, extract the captured group
-        if match:
-            edibletext = match.group(1)
-            edibleuses = edibletext
-            edibleuses = re.sub('\[.*?\]', ' ', edibleuses)
-            medicinal_text = match.group(2)
-            medprop=(medicinal_text)
-            medprop = re.sub('\[.*?\]', ' ', medprop)
-        # Initialize variables to keep track of h3 tags
-    
-        # Print the data
-        # print(edibleuses)
-        
-    except:
-        print("Error123 ")
-    
-    #edible uses if available
-    element2 = soup.find(class_='boots3')
-    if element2:
-        results2 = element2.find('span')
-        resarr = list(results2.descendants)
+        #composition 
         try:
-            edibleuses = resarr[-1]
-            edibleuses = re.sub('\[.*?\]', ' ', edibleuses)
+            composition=list()
+            compositiontable=tables[4]
+            coldata=(compositiontable.find_all('tr'))
+            for row in coldata:
+                rowdata=(row.find_all('td'))
+                individualrowdata=[data.text.strip() for data in rowdata]
+                composition.append(individualrowdata)
+            # print(composition)
+            dry_weight_index = composition.index(['Figures in grams (g) or miligrams (mg) per 100g of food.', ''])
+
+            # Extract the relevant data
+            desired_data = composition[dry_weight_index + 1:]
+        
+            # Format and print the data
+            # for line in desired_data:
+            #     print('\n'.join(line))
+
+            formatted_data = "\n".join(["\n".join(line) for line in desired_data])
+            # print(formatted_data)
+            # Check if the element with class 'boots2' exists
+            string_length = len(formatted_data)
+
+            # Calculate the number of characters to keep (1/3 of the length)
+            characters_to_keep = string_length // 3
+
+            # Slice the string to keep only the first 1/3 of the characters
+            formatted_data = formatted_data[:characters_to_keep]
+            formatted_data =formatted_data.replace("Reference: [ ]Notes", "")
         except IndexError:
-           print("no uses")
+            formatted_data = comp[(species)]
+            # if formatted_data == "Details Not Found":
+            #     formatted_data == comp[(species)]
+        except ValueError or KeyError:
+            formatted_data = comp[(species)]
+            # if formatted_data == "Details Not Found":
+            #     formatted_data == comp[(species)]
+        
+            
+
+        # #medicinal props
+        # element = soup.find(class_='boots2')
+        # if element:
+        #     results = element.find('span')
+        #     resarr = list(results.descendants)
+        #     try:
+        #         medprop = resarr[-1]
+        #         medprop = re.sub('\[.*?\]', ' ', medprop)
+        #     except IndexError:
+        #         medprop = "No Medicinal Properties Available"
+        # else:
+        #     medprop = "No Medicinal Properties Available"
+
+
+        #medicinal props
+        #edible uses if available
+
+        element = soup.find(class_='boots2')
+        try:
+            if element:
+                results = element.find('span')
+                resarr = list(results.descendants)
+                try:
+                    medprop = resarr[-1]
+                    medprop = re.sub('\[.*?\]', ' ', medprop)
+                except IndexError:
+                    medprop = "No Medicinal Properties Available"
+        except Exception as e:
+            print(f"Error parsing first website: {str(e)}")
+        try:
+            page2 = requests.get(url2,headers=headers)
+            soup2 = BeautifulSoup(page2.content, 'html.parser')
+            
+            pagetags=soup2.find('div',class_='PageBox')
+            
+            paragraphs = soup2.find_all('p')  # Assuming the text is within a <p> tag
+            for div in soup2.find_all('div', class_='ref'):
+                div.decompose()
+            text=""
+            for paragraph in paragraphs:
+                text=text+(paragraph.text)
+            
+            # print(text)
+            # Define a regular expression pattern to capture the desired text
+            pattern = r'Edible Uses(.+?)Medicinal(.+?)Other Uses'
+
+            # Use re.search to find the first match
+            match = re.search(pattern, text, re.DOTALL)
+
+            # If a match is found, extract the captured group
+            if match:
+                edibletext = match.group(1)
+                edibleuses = edibletext
+                edibleuses = re.sub('\[.*?\]', ' ', edibleuses)
+                medicinal_text = match.group(2)
+                medprop=(medicinal_text)
+                medprop = re.sub('\[.*?\]', ' ', medprop)
+            # Initialize variables to keep track of h3 tags
+            
+        
+
+            # Print the data
+            # print(edibleuses)
+            
+        except:
+            print("Error123 ")
+        
+        #edible uses if available
+        element2 = soup.find(class_='boots3')
+        if element2:
+            results2 = element2.find('span')
+            resarr = list(results2.descendants)
+            try:
+                edibleuses = resarr[-1]
+                edibleuses = re.sub('\[.*?\]', ' ', edibleuses)
+            except IndexError:
+                print("no uses")
+        else:
+            print("no uses")  
+        # Define filename here before the return statement
+        filename = file_img.filename[:-3] + "jpg"
+        sci_name = sci_name.replace('+', ' ')
+
+        return render_template("pred.html", medprop=medprop, species=species, sci_name=sci_name, img=filename,formatted_data=formatted_data,edibleuses=edibleuses)
     else:
-       print("no uses")  
-    # Define filename here before the return statement
-    filename = file_img.filename[:-3] + "jpg"
-    sci_name = sci_name.replace('+', ' ')
-
-    return render_template("pred.html", medprop=medprop, species=species, sci_name=sci_name, img=filename,formatted_data=formatted_data,edibleuses=edibleuses)
-
+        return render_template("notfound.html")
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
